0caf4aac6abe6be1e20dc2ac299ba2caf7c64e21
AlexanderBoyko24 <160917133+AlexanderBoyko24@users.noreply.github.com>
2024-02-29 22:01:59 +0300

    added .idea to .gitignore

---
+++ src/Lesson2/ShortHandAndOrSideEffects.java
@@ -0,0 +1,14 @@
+package Lesson2;
+
+public class ShortHandAndOrSideEffects {
+    public static void main(String[] args){
+        int i;
+
+        i = 0;
+
+        if(false && (++i < 100))
+            System.out.println("THIS STRING WONT BE DISPLAYED");
+
+        System.out.println("The if operator was executed: " + i);
+    }
+}
