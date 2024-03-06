cdb9b1121aa1f0e74a1a12fd2d2631b29f68cbf6
AlexanderBoyko24 <160917133+AlexanderBoyko24@users.noreply.github.com>
2024-03-06 17:48:40 +0300

    New files

---
+++ src/Exercise3/EX5.java
@@ -0,0 +1,10 @@
+package Exercises4;
+
+public class EX5 {
+    public static void main (String [] args){
+
+
+        System.out.println("one" + "\t" + "two" +"\t" + "three" + "\t");
+
+    }
+}
---
+++ src/Exercise3/Logical2.java
@@ -0,0 +1,32 @@
+package Exercises4;
+
+public class Logical2 {
+    public static void main (String [] args){
+        boolean p, q;
+        p = true;
+        q = false;
+    /*    int x, y;
+
+        x = 1;
+        p = (boolean) x;
+
+        y = 0;
+        q = (boolean) y;*/
+        System.out.println("P\tQ\tAND\tOR\tXOR\tNOT");
+
+        p = true; q =true;
+        if( p & q) System.out.println("1");
+        if(p | q) System.out.println("1");
+        if(p ^ q) System.out.println("0");
+        if(!p) System.out.println("0");
+
+        p = true; q =false;
+        if( p & q) System.out.println(0);
+        if(p | q) System.out.println(1);
+        if(p ^ q) System.out.println(0);
+        if(!p) System.out.println(0);
+
+
+    }
+
+}
