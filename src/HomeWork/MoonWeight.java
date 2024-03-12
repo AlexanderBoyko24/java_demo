f1f048f107348eed64b24b0b09ce1960327abf1f
AlexanderBoyko24 <160917133+AlexanderBoyko24@users.noreply.github.com>
2024-03-12 18:40:09 +0300

    changes

---
+++ src/HomeWork/MoonWeight.java
@@ -0,0 +1,13 @@
+package HomeWork;
+
+public class MoonWeight {
+    public static void main (String args []) {
+        double earthweight;
+        double moonweight;
+
+        earthweight = 80.0;
+         moonweight = earthweight * 0.17;
+
+        System.out.println("Мой вес на Луне был бы: " + moonweight);
+    }
+}
