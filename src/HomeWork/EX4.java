e183b8ee7481991e6c22679ecdb49a311428da8b
AlexanderBoyko24 <160917133+AlexanderBoyko24@users.noreply.github.com>
2024-03-13 09:46:25 +0300

    changes

--- src/HomeWork/EX4.java
+++ src/HomeWork/EX4.java
@@ -3,8 +3,9 @@ package HomeWork;
 public class EX4 {
     public static void main (String [] args){
 
-        System.out.println("One");
-        System.out.println("Two");
-        System.out.println("Three");
+        System.out.println("One\nTwo\nThree");
+
+
+
     }
 }
---
+++ src/HomeWork/EX5.java
@@ -0,0 +1,12 @@
+package HomeWork;
+
+public class EX5 {
+    public static void main (String [] args){
+        System.out.println("One\tTwo\tThree");
+        System.out.println();
+        System.out.println("One\nTwo\nThree");
+        System.out.println();
+        System.out.println("one" + "\t" + "two" +"\t" + "three" + "\t");
+
+    }
+}
