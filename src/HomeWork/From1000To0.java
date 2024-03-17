fbe060427c4af9090c92dd1395648cc45305dbde
AlexanderBoyko24 <160917133+AlexanderBoyko24@users.noreply.github.com>
2024-03-17 13:38:29 +0300

    new files

---
+++ src/HomeWork/From1000To0.java
@@ -0,0 +1,12 @@
+package HomeWork;
+
+public class From1000To0 {
+    public static void main (String [] args){
+        int i;
+
+        for (i = 1000; i >= 0; i-=2) {
+            System.out.println("i is: "+ i);
+
+        }
+    }
+}
