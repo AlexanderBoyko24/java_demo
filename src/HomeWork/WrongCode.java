5dcfbdf43ba3ec639da113250343ced2c774ba46
AlexanderBoyko24 <160917133+AlexanderBoyko24@users.noreply.github.com>
2024-03-17 13:58:38 +0300

    new files

---
+++ src/HomeWork/WrongCode.java
@@ -0,0 +1,17 @@
+package HomeWork;
+
+public class WrongCode {
+    public static void main(String[] args) {
+        int count = 0;
+        int sum = 0;
+        int num = 20;
+
+        for (int i = 0; i < num; i++) {
+            sum += i;
+            count = i;
+            System.out.println( + i + sum + count);
+        }
+
+    }
+}
+// вопрос: что тут считается
