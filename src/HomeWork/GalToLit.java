e460073cfbb999d13e10db4dfa94007d59976b9d
AlexanderBoyko24 <160917133+AlexanderBoyko24@users.noreply.github.com>
2024-03-21 14:41:33 +0300

    new files

--- src/HomeWork/GalToLit.java
+++ src/HomeWork/GalToLit.java
@@ -2,23 +2,26 @@ package HomeWork;
 
 import java.util.Scanner;
 
-public class GalToLit{
-    public static void main (String [] args) {
+public class GalToLit {
+    public static void main(String[] args) {
 
 
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter your number:");
-        double gallons;
 
-        do   {
+        System.out.println("Ведите число:");
+        while (true) {
+            double gallons;
             gallons = input.nextDouble();
             double liters;
-
             liters = gallons * 3.7854;
 
-            System.out.println(gallons + " галлонам соответсвует " + liters + " литров");
-            // System.out.println("Enter another number"); - как зациклить эту программу, чтобы я мог вводить другие числа?
-        } while (gallons > 0);
+            if (gallons <=0) break;
 
+            System.out.println(gallons + " галлонам соответсвует " + liters + " литров");
+            System.out.println();
+          System.out.println("Введите другое число, чтобы продолжить работу.");
+          System.out.println("Введите 0, чтобы завершить работу.");
+
+        }
     }
 }
