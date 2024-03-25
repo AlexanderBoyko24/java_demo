9ba3eef311b850e1947c8c9be95e7b3714f48bdf
AlexanderBoyko24 <160917133+AlexanderBoyko24@users.noreply.github.com>
2024-03-25 19:18:04 +0300

    new files

---
+++ src/HomeWork/SquareAreaCalculation.java
@@ -0,0 +1,13 @@
+package HomeWork;
+
+public class SquareAreaCalculation{
+    public static void main (String [] args){
+
+        Square square1 = new Square(12,5);
+        System.out.println("Square area: " + square1.area());
+
+        Square square2 = new Square(3,9);
+        System.out.println("Square area: " + square2.area());
+
+    }
+}
